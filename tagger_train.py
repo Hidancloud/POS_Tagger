290102c9f6ca21e2db953202308a00507d9c43bb
HidanCloud <hidancloud@yandex.ru>
2020-02-21 03:26:34 +0400

    model, data embedding and etc. todo - code refactoring, code train process and save the model

--- tagger_train.py
+++ tagger_train.py
@@ -16,6 +16,7 @@ from multiprocessing import cpu_count
 all_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .!?:,\'%-\(\)/$|&;[]"{}#*=@`'
 
 
+
 def prepare_sequence(seq, to_ix):
     idxs = []
     for word in seq:
@@ -63,35 +64,78 @@ def load_embeddings(data, word_dict, size=50):
     return word_embeddings
 
 
-def char_embedding():
+def char_embedding(chars, emb_size=10):
     '''embeddings = np.array([hash(c) for c in s])
     embeddings /= np.max(embeddings)
     embeddings = {s[i]: embeddings[i] for i in range(len(embeddings))}'''
 
     char_embeddings = dict()
-    for i, c in enumerate(all_chars):
-        char_embeddings[c] = [0]*len(all_chars)
+    for i, c in enumerate(chars):
+        char_embeddings[c] = [0]*len(chars)
         char_embeddings[c][i] = 1
-    for c in char_embeddings.keys():
-        print(c)
-        print(char_embeddings[c])
-        print('\n')
+    # torch.nn.Embedding(len(char_embeddings.keys()), emb_size)
+
     return char_embeddings
 
 
-def train_model(train_file, model_file=0):
-    # write your code here. You can add functions as well.
-    # use torch library to save model parameters, hyperparameters, etc. to model_file
+def train_model(train_file, model_file='0'):
+    maxlen = -float('inf')
     training_data, tags_set = read_data(train_file)
     chars = dict()
     word_to_ix = {}
     for sent, tags in training_data:
+        print(sent)
         for word in sent:
             if word not in word_to_ix:
                 word_to_ix[word] = len(word_to_ix)
+            if len(word) > maxlen:
+                maxlen = len(word)
 
+    ''' unused part, skip
     word_embeddings = load_embeddings([i[0] for i in training_data], word_to_ix)
-    char_embeddings = char_embedding()
+    char_embeddings = char_embedding(all_chars) '''
+
+    # parameters from the task
+    l = 5
+    w_emb = 50
+    d_char = 10
+    k = 5
+    torch_word_embedding = nn.Embedding(len(word_to_ix), w_emb)
+    torch_char_embedding = nn.Embedding(len(all_chars), d_char)
+
+    for sent, _ in training_data:
+        for word in sent:
+            # create embedings for the word and each char in it
+            this_word_embedding = torch_word_embedding(torch.tensor([word_to_ix[word]], dtype=torch.long))
+            char_embeddings = [torch_char_embedding(torch.tensor([all_chars.index(char)], dtype=torch.long)) for char in word]
+            # pad zeros to the left and to the right to correctly apply method on bounds
+            char_embeddings.append([[0]*d_char]*int((k-1)/2))
+            char_embeddings = [[0]*d_char]*int((k-1)/2) + char_embeddings
+            # create X vector from the window size K
+            X = [char_embeddings[i-int((k-1)/2):i+int((k-1)/2)] for i in range(int((k-1)/2), len(char_embeddings) - int((k-1)/2))]
+            # apply g(XU + b) from the task **what is g by the way?**
+            linear = nn.Linear(k*d_char, l, bias=True)
+            proceed = np.array([torch.nn.functional.relu(linear(x_i)) for x_i in X])
+            # calculate c using the formula from the task
+            c = np.array([max(proceed[:, k]) for k in range(l)])
+            # final representation of the word, concat word embedding and c
+            x = [*this_word_embedding, *c]
+            # apply bidirectional lstm, to extract both hidden states two models are used, can be replaced with one
+            # with flag *bidirectional* = True
+            lstm_forward = nn.LSTM(len(x), hidden_size=50, num_layers=1, bias=True, batch_first=False, dropout=0, bidirectional=False)
+            _, hidden_forward = lstm_forward(x)
+            lstm_backward = nn.LSTM(len(x), hidden_size=50, num_layers=1, bias=True, batch_first=False, dropout=0, bidirectional=False)
+            x.reverse()
+            _, hidden_backward = lstm_backward(x)
+            h = np.array(*hidden_forward, *hidden_backward)
+            # projecting vector of bidirectional lstm with size 50+50=100 on the correct space with dim=45 using linear
+            # layer
+            last_layer = nn.Linear(2*50, 45, bias=True)
+            answer = torch.nn.functional.softmax(last_layer(h))
+
+            ### TODO: use Adam classifier to optimize everything with nn.NLLLoss, calculate accuracy and save the model
+            ### TODO: also test everything
+
     tag_to_ix = {}
     ix_to_tag = {}
     count = 0
