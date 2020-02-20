9c5e345df47a34efb812c892e0c6db06e88b731f
HidanCloud <hidancloud@yandex.ru>
2020-02-20 19:56:31 +0400

    char embedding, elementary approach

--- tagger_train.py
+++ tagger_train.py
@@ -1,16 +1,19 @@
 # python3.7 tagger_train.py <train_file_absolute_path> <model_file_absolute_path>
 
-import pickle
-import sys
 from random import uniform
-
-import numpy as np
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import pickle
+
+import sys
+import numpy as np
+import torch
 import random
 from gensim.models import Word2Vec
+from multiprocessing import cpu_count
+
+all_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .!?:,\'%-\(\)/$|&;[]"{}#*=@`'
 
 
 def prepare_sequence(seq, to_ix):
@@ -48,31 +51,39 @@ def read_data(path):
     return training_data, tags_set
 
 
-def load_embeddings(data, word_dict):
-    model = Word2Vec(data, min_count=1, size=50, workers=3, window=3, sg=1)
-    word_embeddings = np.zeros((len(word_dict), 50))
-
+def load_embeddings(data, word_dict, size=50):
+    model = Word2Vec(data, min_count=1, size=size, workers=cpu_count(), window=3, sg=1)
+    word_embeddings = np.zeros((len(word_dict), size))
     for word in word_dict:
         word_embeddings[word_dict[word]] = model[word]
-        print(word)
-        print(model[word])
+        #print(word)
+        #print(model[word])
 
+    # print(model.most_similar('gunshot')[:3])
     return word_embeddings
 
 
-def char_embeddings():
-    s = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .!?:,\'%-\(\)/$|&;[]"'
-    embeddings = np.array([hash(c) for c in s])
+def char_embedding():
+    '''embeddings = np.array([hash(c) for c in s])
     embeddings /= np.max(embeddings)
-    embeddings = {s[i]: embeddings[i] for i in range(len(embeddings))}
-    return embeddings
+    embeddings = {s[i]: embeddings[i] for i in range(len(embeddings))}'''
+
+    char_embeddings = dict()
+    for i, c in enumerate(all_chars):
+        char_embeddings[c] = [0]*len(all_chars)
+        char_embeddings[c][i] = 1
+    for c in char_embeddings.keys():
+        print(c)
+        print(char_embeddings[c])
+        print('\n')
+    return char_embeddings
 
 
-def train_model(train_file, model_file):
+def train_model(train_file, model_file=0):
     # write your code here. You can add functions as well.
     # use torch library to save model parameters, hyperparameters, etc. to model_file
     training_data, tags_set = read_data(train_file)
-
+    chars = dict()
     word_to_ix = {}
     for sent, tags in training_data:
         for word in sent:
@@ -80,7 +91,7 @@ def train_model(train_file, model_file):
                 word_to_ix[word] = len(word_to_ix)
 
     word_embeddings = load_embeddings([i[0] for i in training_data], word_to_ix)
-
+    char_embeddings = char_embedding()
     tag_to_ix = {}
     ix_to_tag = {}
     count = 0
@@ -90,6 +101,7 @@ def train_model(train_file, model_file):
         count += 1
 
     print('Finished...')
+
 
 
 if __name__ == "__main__":
